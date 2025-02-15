00c390b36e021c6c2a9f2568404d6ce6ed5eb37d
juanmata06 <mataguzzojuan@gmail.com>
2025-02-15 19:41:07 +0100

    first component working and tailwind installed

--- projects/jamg-angular/ngx-components/src/lib/top-button/top-button.component.spec.ts
+++ projects/jamg-angular/ngx-components/src/lib/top-button/top-button.component.spec.ts
@@ -1,23 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ViewportScroller } from '@angular/common';
 import { TopButtonComponent } from './top-button.component';
 
 describe('TopButtonComponent', () => {
   let component: TopButtonComponent;
   let fixture: ComponentFixture<TopButtonComponent>;
+  let compiled: HTMLElement;
+  let viewportScroller: ViewportScroller;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TopButtonComponent]
-    })
-    .compileComponents();
-    
+      imports: [TopButtonComponent],
+      providers: [{ provide: ViewportScroller, useValue: { getScrollPosition: () => [0, 0], scrollToPosition: jasmine.createSpy() } }]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TopButtonComponent);
     component = fixture.componentInstance;
+    viewportScroller = TestBed.inject(ViewportScroller);
+    compiled = fixture.nativeElement as HTMLElement;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display button when scroll height exceeds threshold', () => {
+    spyOn(viewportScroller, 'getScrollPosition').and.returnValue([0, 201]);
+    component.onWindowScroll();
+    expect(component.isDisplayingButton()).toBeTrue();
+  });
+
+  it('should hide button when scroll height is below threshold', () => {
+    spyOn(viewportScroller, 'getScrollPosition').and.returnValue([0, 200]);
+    component.onWindowScroll();
+    expect(component.isDisplayingButton()).toBeFalse();
+  });
+
+  it('should scroll to top when button is clicked', () => {
+    component.scrollToTop();
+    expect(viewportScroller.scrollToPosition).toHaveBeenCalledWith([0, 0]);
+  });
+
+  it('should apply correct variant class', () => {
+    component.variant = 'primary';
+    fixture.detectChanges();
+    const hostCssClasses: string[] = compiled.classList.value.split(' ');
+    expect(hostCssClasses).toContain('button-primary');
   });
 });
--- projects/jamg-angular/ngx-components/src/lib/top-button/top-button.component.ts
+++ projects/jamg-angular/ngx-components/src/lib/top-button/top-button.component.ts
@@ -1,12 +1,78 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, Input, signal } from '@angular/core';
 
 @Component({
   selector: 'jamg-top-button',
   standalone: true,
-  imports: [],
   templateUrl: './top-button.component.html',
-  styleUrl: './top-button.component.css'
+  styleUrl: './top-button.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class]': 'buttonStyles()',
+    '[style.display]': 'isDisplayingButton() ? "block" : "none"',
+    '(window:scroll)': 'onWindowScroll()'
+  }
 })
 export class TopButtonComponent {
+  /**
+   * ------------------------------------------------------------------------------------------------------------------------------
+   * General vars
+   * ------------------------------------------------------------------------------------------------------------------------------
+   */
+  private readonly SCROLL_HEIGHT_TO_DISPLAY = 200;
+  private readonly TOP_POSITION: [number, number] = [0, 0];
 
+  @Input() variant: 'primary' | 'secondary' | 'default' = 'default';
+  @Input() text: string = '';
+
+  public buttonText = computed(() => this.text.trim() !== '' ? this.text : '⌃');
+  public isDisplayingButton = signal(false);
+
+  /**
+   * -----------------------------------------------------------------------------------------------------------------------------
+   * LYFECYCLE METHODS
+   * -----------------------------------------------------------------------------------------------------------------------------
+   */
+  constructor(private readonly _viewportScroller: ViewportScroller) { }
+
+  /**
+   * ------------------------------------------------------------------------------------------------------------------------------
+   * PRIVATE METHODS
+   * ------------------------------------------------------------------------------------------------------------------------------
+   */
+
+  /**
+   * ------------------------------------------------------------------------------------------------------------------------------
+   * PRIVATE VALIDATION AND INTERNAL PROCESS METHODS
+   * ------------------------------------------------------------------------------------------------------------------------------
+   */
+  private buttonStyles(): string {
+    const baseClasses = 'fixed bottom-5 right-7 z-50 font-bold py-2 px-4 rounded cursor-pointer transition-all duration-300 ease-in-out';
+    const variants = {
+      primary: 'button-primary',
+      secondary: 'button-secondary',
+      default: 'button-default'
+    };
+    return `${baseClasses} ${variants[this.variant]}`;
+  }
+
+  /**
+   * ------------------------------------------------------------------------------------------------------------------------------
+   * PUBLIC METHODS
+   * ------------------------------------------------------------------------------------------------------------------------------
+   */
+
+  /**
+   * ------------------------------------------------------------------------------------------------------------------------------
+   * PUBLIC VALIDATION AND INTERNAL PROCESS METHODS
+   * ------------------------------------------------------------------------------------------------------------------------------
+   */
+  public onWindowScroll(): void {
+    const [, scrollY] = this._viewportScroller.getScrollPosition();
+    this.isDisplayingButton.set(scrollY > this.SCROLL_HEIGHT_TO_DISPLAY);
+  }
+
+  public scrollToTop(): void {
+    this._viewportScroller.scrollToPosition(this.TOP_POSITION);
+  }
 }
